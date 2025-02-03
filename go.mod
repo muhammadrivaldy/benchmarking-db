5d3657d8470ac8226203d3b32ab9b62ecd66a700
Muhammad Rivaldy Was Here! <muhammadrivaldy16@gmail.com>
2025-02-03 19:53:36 +0700

    Finish to setup the benchmark

--- go.mod
+++ go.mod
@@ -8,6 +8,8 @@ require (
 )
 
 require (
+	github.com/go-sql-driver/mysql v1.5.0 // indirect
+	github.com/golang-migrate/migrate v3.5.4+incompatible // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
--- main.go
+++ main.go
@@ -7,7 +7,9 @@ import (
 	"os"
 	"sync"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/mysql"
 	"github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 	_ "github.com/lib/pq"
@@ -28,30 +30,75 @@ func runService() {
 
 	wg := new(sync.WaitGroup)
 
-	for i := 0; i < 1; i++ {
-		wg.Add(1)
-		go func(wg *sync.WaitGroup) {
-			defer wg.Done()
-			db := openDB()
-			defer db.Close()
+	wg.Add(2)
 
-			for i := 0; i < 1000000; i++ {
-				name := fmt.Sprintf("User_%d", i)
-				address := fmt.Sprintf("Address_%d", i)
-				_, err := db.Exec("INSERT INTO mst_user (name, address) VALUES ($1, $2)", name, address)
-				if err != nil {
-					log.Fatalf("Insert failed: %v", err)
+	n := 5000
+	d := 50
+
+	go func(wg *sync.WaitGroup) {
+
+		childWG := new(sync.WaitGroup)
+
+		for i := 0; i < d; i++ {
+
+			childWG.Add(1)
+
+			go func(childWG *sync.WaitGroup) {
+				defer childWG.Done()
+				db := openPostgresDB()
+				defer db.Close()
+
+				for i := 0; i < n; i++ {
+					name := fmt.Sprintf("User_%d", i)
+					address := fmt.Sprintf("Address_%d", i)
+					_, err := db.Exec("INSERT INTO mst_user (name, address) VALUES ($1, $2)", name, address)
+					if err != nil {
+						log.Fatalf("Insert failed: %v", err)
+					}
 				}
-			}
 
-			fmt.Println("✅ Inserted 1000000 users.")
-		}(wg)
-	}
+				fmt.Printf("✅ PostgreSQL inserted %d users.\n", n)
+			}(childWG)
+		}
+
+		childWG.Wait()
+		wg.Done()
+	}(wg)
+
+	go func(wg *sync.WaitGroup) {
+
+		childWG := new(sync.WaitGroup)
+
+		for i := 0; i < d; i++ {
+
+			childWG.Add(1)
+
+			go func(childWG *sync.WaitGroup) {
+				defer childWG.Done()
+				db := openMySQLDB()
+				defer db.Close()
+
+				for i := 0; i < n; i++ {
+					name := fmt.Sprintf("User_%d", i)
+					address := fmt.Sprintf("Address_%d", i)
+					_, err := db.Exec("INSERT INTO mst_user (name, address) VALUES (?, ?)", name, address)
+					if err != nil {
+						log.Fatalf("Insert failed: %v", err)
+					}
+				}
+
+				fmt.Printf("✅ MySQL inserted %d users.\n", n)
+			}(childWG)
+		}
+
+		childWG.Wait()
+		wg.Done()
+	}(wg)
 
 	wg.Wait()
 }
 
-func openDB() *sql.DB {
+func openPostgresDB() *sql.DB {
 
 	const host = "localhost"
 	const port = 5432
@@ -75,9 +122,38 @@ func openDB() *sql.DB {
 	return db
 }
 
-func runMigration() {
+func openMySQLDB() *sql.DB {
 
-	db := openDB()
+	const host = "localhost"
+	const port = 3306
+	const user = "admin"
+	const password = "admin"
+	const dbname = "mydb"
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", user, password, host, port, dbname)
+
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatalf("Could not connect to database: %v", err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatalf("Database is unreachable: %v", err)
+	}
+
+	fmt.Println("✅ Connected to MySQL!")
+	return db
+}
+
+func runMigration() {
+	migrationPostgresDB()
+	migrationMySQLDB()
+}
+
+func migrationPostgresDB() {
+
+	db := openPostgresDB()
 	defer db.Close()
 
 	driver, err := postgres.WithInstance(db, &postgres.Config{})
@@ -98,5 +174,31 @@ func runMigration() {
 		log.Fatalf("Migration failed: %v", err)
 	}
 
-	fmt.Println("✅ Migrations applied successfully!")
+	fmt.Println("✅ PostgreSQL migrations applied successfully!")
+}
+
+func migrationMySQLDB() {
+
+	db := openMySQLDB()
+	defer db.Close()
+
+	driver, err := mysql.WithInstance(db, &mysql.Config{})
+	if err != nil {
+		log.Fatalf("Could not create driver: %v", err)
+	}
+
+	m, err := migrate.NewWithDatabaseInstance(
+		"file://migrations",
+		"mydb",
+		driver,
+	)
+	if err != nil {
+		log.Fatalf("Migration initialization failed: %v", err)
+	}
+
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		log.Fatalf("Migration failed: %v", err)
+	}
+
+	fmt.Println("✅ MySQL migrations applied successfully!")
 }
