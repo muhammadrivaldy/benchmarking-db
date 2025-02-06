3e87c672fd33d76b66699e2192cbd009c597ff06
Muhammad Rivaldy Was Here! <muhammadrivaldy16@gmail.com>
2025-02-06 08:32:44 +0700

    Updating config of mysql & postgres

--- main.go
+++ main.go
@@ -30,72 +30,83 @@ func runService() {
 
 	wg := new(sync.WaitGroup)
 
-	wg.Add(2)
+	n := 10
+	d := 5
+	iteration := 1
 
-	n := 5000
-	d := 50
+	for {
 
-	go func(wg *sync.WaitGroup) {
+		wg.Add(2)
 
-		childWG := new(sync.WaitGroup)
+		go func(wg *sync.WaitGroup) {
 
-		for i := 0; i < d; i++ {
+			childWG := new(sync.WaitGroup)
 
-			childWG.Add(1)
+			for i := 0; i < d; i++ {
 
-			go func(childWG *sync.WaitGroup) {
-				defer childWG.Done()
-				db := openPostgresDB()
-				defer db.Close()
+				childWG.Add(1)
 
-				for i := 0; i < n; i++ {
-					name := fmt.Sprintf("User_%d", i)
-					address := fmt.Sprintf("Address_%d", i)
-					_, err := db.Exec("INSERT INTO mst_user (name, address) VALUES ($1, $2)", name, address)
-					if err != nil {
-						log.Fatalf("Insert failed: %v", err)
+				go func(childWG *sync.WaitGroup) {
+					defer childWG.Done()
+					db := openPostgresDB()
+					defer db.Close()
+
+					for i := 0; i < n; i++ {
+						name := fmt.Sprintf("User_%d", i)
+						address := fmt.Sprintf("Address_%d", i)
+						_, err := db.Exec("INSERT INTO mst_user (name, address) VALUES ($1, $2)", name, address)
+						if err != nil {
+							log.Fatalf("Insert failed: %v", err)
+						}
 					}
-				}
+				}(childWG)
+			}
 
-				fmt.Printf("✅ PostgreSQL inserted %d users.\n", n)
-			}(childWG)
-		}
+			childWG.Wait()
+			wg.Done()
+		}(wg)
 
-		childWG.Wait()
-		wg.Done()
-	}(wg)
+		go func(wg *sync.WaitGroup) {
 
-	go func(wg *sync.WaitGroup) {
+			childWG := new(sync.WaitGroup)
 
-		childWG := new(sync.WaitGroup)
+			for i := 0; i < d; i++ {
 
-		for i := 0; i < d; i++ {
+				childWG.Add(1)
 
-			childWG.Add(1)
+				go func(childWG *sync.WaitGroup) {
+					defer childWG.Done()
+					db := openMySQLDB()
+					defer db.Close()
 
-			go func(childWG *sync.WaitGroup) {
-				defer childWG.Done()
-				db := openMySQLDB()
-				defer db.Close()
-
-				for i := 0; i < n; i++ {
-					name := fmt.Sprintf("User_%d", i)
-					address := fmt.Sprintf("Address_%d", i)
-					_, err := db.Exec("INSERT INTO mst_user (name, address) VALUES (?, ?)", name, address)
-					if err != nil {
-						log.Fatalf("Insert failed: %v", err)
+					for i := 0; i < n; i++ {
+						name := fmt.Sprintf("User_%d", i)
+						address := fmt.Sprintf("Address_%d", i)
+						_, err := db.Exec("INSERT INTO mst_user (name, address) VALUES (?, ?)", name, address)
+						if err != nil {
+							log.Fatalf("Insert failed: %v", err)
+						}
 					}
-				}
+				}(childWG)
+			}
 
-				fmt.Printf("✅ MySQL inserted %d users.\n", n)
-			}(childWG)
+			childWG.Wait()
+			wg.Done()
+		}(wg)
+
+		wg.Wait()
+
+		fmt.Printf("✅ Iteration %d is done!\n", iteration)
+		fmt.Printf("✅ %d records inserted to PostgreSQL\n", (n * d))
+		fmt.Printf("✅ %d records inserted to MySQL\n", (n * d))
+
+		n += 500
+		d += 2
+		iteration++
+		if iteration > 20 {
+			break
 		}
-
-		childWG.Wait()
-		wg.Done()
-	}(wg)
-
-	wg.Wait()
+	}
 }
 
 func openPostgresDB() *sql.DB {
@@ -118,7 +129,6 @@ func openPostgresDB() *sql.DB {
 		log.Fatalf("Database is unreachable: %v", err)
 	}
 
-	fmt.Println("✅ Connected to PostgreSQL!")
 	return db
 }
 
@@ -142,7 +152,6 @@ func openMySQLDB() *sql.DB {
 		log.Fatalf("Database is unreachable: %v", err)
 	}
 
-	fmt.Println("✅ Connected to MySQL!")
 	return db
 }
 
